bdca7e529454f529dd7e8cdcdeaaece7ed498bb9
Anna Khramova <a.hramova@astondevs.ru>
2024-08-21 23:29:42 +0300

    Add Test

---
+++ src/test/java/com/example/task_9_1/ApplicationTests.java
@@ -0,0 +1,34 @@
+package com.example.task_9_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Container
+    private final GenericContainer<?> devApp = new GenericContainer<>("devapp:latest")
+            .withExposedPorts(8080);
+
+    @Container
+    private final GenericContainer<?> prodApp = new GenericContainer<>("prodapp:latest")
+            .withExposedPorts(8081);
+
+    @Test
+    void contextLoads() {
+        ResponseEntity<String> forEntityFromDev = restTemplate.getForEntity("http://localhost:" + devApp.getMappedPort(8080) + "/profile", String.class);
+        ResponseEntity<String> forEntityFromProd = restTemplate.getForEntity("http://localhost:" + prodApp.getMappedPort(8081) + "/profile", String.class);
+        System.out.println(forEntityFromDev.getBody());
+        System.out.println(forEntityFromProd.getBody());
+    }
+
+}
